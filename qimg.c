6fce286345155ea2b7a076566e9abe017db644f4
Joni Lepistö <joni.m.lepisto@gmail.com>
2020-11-10 13:15:15 +0200

    Initial project commit

---
+++ qimg.c
@@ -0,0 +1,282 @@
+/**
+ ** This file is part of the qimg project.
+ ** Copyright 2020 Joni Lepistö <joni.m.lepisto@gmail.com>.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ** ----------------------------------------------------------------------------
+ **
+ ** Qimg - Quick Image Display
+ **
+ ** Qimg provides a totally stripped and straightforward way of displaying
+ ** images on a Linux system. No desktop environment or windowing system needed!
+ **
+ ** Qimg uses the Linux framebuffer interface to draw bitmap data on the screen.
+ ** Images are drawn as raw pixels with no windowing context whatsoever.
+ **
+ ** Why? Mostly for fun but I've had a few occasions on some terminal-only
+ ** systems where it would've been nice to inspect images.
+ **/
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <assert.h>
+#include <fcntl.h>
+#include <glob.h>
+#include <stdint.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <linux/fb.h>
+
+#define FB_IDX_MAX_SIZE 4
+#define FB_DEV_BASE "/dev/fb"
+#define FB_CLASS_BASE "/sys/class/graphics/fb"
+#define FB_CLASS_RESOLUTION "/virtual_size"
+#define FB_GLOB "/sys/class/graphics/fb[0-9]"
+
+/* Standard terminal control sequences */
+#define CUR_SHOW "\e[?25h"
+#define CUR_HIDE "\e[?25l"
+
+#define log_error(M) fprintf(stderr, "[ERROR]: %s\n", M)
+#define assertf(A, M) if (!(A)) {log_error(M); exit(EXIT_FAILURE);} void f(void)
+
+typedef enum { false, true } bool;
+
+typedef struct res_ {
+    int x;
+    int y;
+} qimg_resolution;
+
+typedef struct col_ {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+} qimg_color;
+
+typedef struct fb_ {
+    qimg_resolution res;/* framebuffer resolution */
+    unsigned int size;  /* framebuffer size */
+    int fbfd;           /* framebuffer file descriptor */
+    char* fbdata;       /* framebuffer data pointer */
+} qimg_fb;
+
+typedef struct im_ {
+    qimg_resolution res;/* resolution */
+    int c;              /* channels */
+    char _padding[4];   /* guess what */
+    uint8_t* pixels;    /* image data pointer */
+} qimq_image;
+
+static volatile bool run = true; /* used to go through cleanup on exit */
+
+qimg_color qimg_get_pixel_color(qimq_image* im, int x, int y);
+qimq_image qimg_load_image(char* input_path);
+qimg_fb qimg_open_framebuffer(int idx);
+void qimg_free_framebuffer(qimg_fb* fb);
+void qimg_free_image(qimq_image* im);
+void qimg_draw_image(qimq_image* im, qimg_fb* fb, bool repaint);
+
+int get_default_framebuffer_idx(void);
+void set_cursor_visibility(bool blink);
+void enable_cursor(void);
+void interrupt_handler(int);
+void print_help(void);
+
+int get_default_framebuffer_idx() {
+    /* Glob search for framebuffer devices */
+    glob_t globbuf;
+    glob(FB_GLOB, 0, NULL, &globbuf);
+
+    /* At least one result matched */
+    assertf(globbuf.gl_pathc > 0, "No framebuffers found");
+
+    /* Get framebuffer index */
+    strsep(&globbuf.gl_pathv[0], "fb"); /* Discard the first split */
+    return atoi(strsep(&globbuf.gl_pathv[0], "fb"));
+}
+
+void qimg_free_framebuffer(qimg_fb* fb) {
+    munmap(fb->fbdata, fb->size);
+    close(fb->fbfd);
+}
+
+qimg_fb qimg_open_framebuffer(int idx) {
+    /* Append device index to the framebuffer device path */
+    char idx_buf[FB_IDX_MAX_SIZE];
+    assertf(snprintf(idx_buf, FB_IDX_MAX_SIZE, "%d", idx) <
+            FB_IDX_MAX_SIZE, "Framebuffer index overflow");
+    char dev[FB_IDX_MAX_SIZE + sizeof(FB_DEV_BASE)] = FB_DEV_BASE;
+    strncat(dev, idx_buf, FB_IDX_MAX_SIZE);
+
+    /* Open framebuffer */
+    qimg_fb fb;
+    fb.fbfd = open(dev, O_RDWR);
+    assertf(fb.fbfd >= 0, "Framebuffer device fopen() failed");
+
+    /* Get framebuffer information */
+    struct fb_var_screeninfo vinfo;
+    ioctl(fb.fbfd, FBIOGET_VSCREENINFO, &vinfo);
+
+    fb.res.x = (int) vinfo.xres;
+    fb.res.y = (int) vinfo.yres;
+    unsigned int fb_bpp = vinfo.bits_per_pixel;
+    unsigned int fb_bytes = fb_bpp / 8;
+
+    /* Calculate data size and map framebuffer to memory */
+    fb.size = fb.res.x * fb.res.y * fb_bytes;
+    fb.fbdata = mmap(0, fb.size,
+                        PROT_READ | PROT_WRITE, MAP_SHARED, fb.fbfd, (off_t) 0);
+
+    return fb;
+}
+
+void qimg_draw_image(qimq_image* im, qimg_fb* fb, bool repaint) {
+    char* buf = malloc(fb->size);
+    qimg_color c;
+    int offs;
+    for (int x = 0; x < fb->res.x; ++x) { /* This is embarassingly parallel */
+        for (int y = 0; y < fb->res.y; ++y) {
+            if (x > im->res.x || y > im->res.y)
+                continue;
+            c = qimg_get_pixel_color(im, x, y);
+            offs = (y * fb->res.x + x) * 4;
+            buf[offs + 0] = (char) c.b;
+            buf[offs + 1] = (char) c.g;
+            buf[offs + 2] = (char) c.r;
+            buf[offs + 3] = (char) c.a;
+        }
+    }
+
+    do {
+        memcpy(fb->fbdata, buf, fb->size);
+    } while (repaint && run);
+}
+
+qimg_color qimg_get_pixel_color(qimq_image* im, int x, int y) {
+    assertf(x <= im->res.x && y <= im->res.y, "Image coordinates out of bounds");
+    uint8_t* offset = im->pixels + (y * im->res.x + x) * im->c;
+    qimg_color color;
+    color.r = offset[0];
+    color.g = offset[1];
+    color.b = offset[2];
+    color.a = im->c >= 4 ? offset[3] : 0xff;
+    return color;
+}
+
+qimq_image qimg_load_image(char* input_path) {
+    qimq_image im;
+    im.pixels = stbi_load(input_path, &im.res.x, &im.res.y, &im.c, 0);
+    assertf(im.pixels, "Image loading failed");
+    return im;
+}
+
+void qimg_free_image(qimq_image* im) {
+    if (im->pixels)
+    free(im->pixels);
+}
+
+void set_cursor_visibility(bool visible) {
+    if (visible) printf(CUR_SHOW);
+    else printf(CUR_HIDE);
+    fflush(stdout);
+}
+
+void enable_cursor() {
+    set_cursor_visibility(true);
+}
+
+void interrupt_handler(int dummy) {
+    run = false;
+}
+
+void print_help() {
+    printf("QIMG - Quick Image Display\n"
+           "\n"
+           "Usage: qimg [OPTION]... INPUT...\n"
+           "\n"
+           "Options:\n"
+           "-h,             Print this help\n"
+           "-b <index>,     Use framebuffer device with given index\n"
+           "-c,             Hide terminal cursor\n"
+           "-r,             Keep repainting the image. If hiding the cursor\n"
+           "                fails, this will certainly work for keeping the\n"
+           "                image on top\n");
+}
+
+void parse_arguments(int argc, char *argv[], int* fb_idx, char** input,
+                     bool* refresh, bool* hide_cursor) {
+    assertf(argc > 1, "Arguments missing");
+    int opts = 0;
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-b") == 0) {
+            ++opts;
+            if (argc > (++i)) {
+                ++opts;
+                int temp = atoi(argv[i]);
+                *fb_idx = temp;
+            }
+        } else if (strcmp(argv[i], "-r") == 0) {
+            ++opts;
+            *refresh = true;
+        } else if (strcmp(argv[i], "-c") == 0) {
+            *hide_cursor = true;
+        } else if (strcmp(argv[i], "-h") == 0) {
+            print_help();
+            exit(0);
+        }
+    }
+
+    if (++opts < argc)
+        *input = argv[argc-1];
+}
+
+int main(int argc, char *argv[]) {
+    int fb_idx = -1;
+    char* input_path = NULL;
+    bool repaint = false;
+    bool hide_cursor = false;
+    parse_arguments(argc, argv, &fb_idx, &input_path, &repaint, &hide_cursor);
+
+    assertf(input_path, "No input file");
+    if (fb_idx == -1)
+        fb_idx = get_default_framebuffer_idx();
+
+    qimg_fb fb = qimg_open_framebuffer(fb_idx);
+    qimq_image im = qimg_load_image(input_path);
+
+    /* Setup exit hooks on signals */
+    signal(SIGINT, interrupt_handler);
+    signal(SIGTERM, interrupt_handler);
+
+    /* Fasten your seatbelts */
+    if (hide_cursor) set_cursor_visibility(false);
+    qimg_draw_image(&im, &fb, repaint);
+
+    /* Pause to keep terminal cursor hidden */
+    /* Pause will return when a signal is caught AND handled */
+    if (!repaint && hide_cursor) pause();
+
+    /* Cleanup */
+    if (hide_cursor) set_cursor_visibility(true);
+    qimg_free_image(&im);
+    qimg_free_framebuffer(&fb);
+
+	return 0;
+}
